494e0cfbb30be9808873d6bc08d92739044b5298
EduardoCosta007 <profissionaleduardocostaa@gmail.com>
2024-03-11 22:48:40 -0300

    alterações

--- js/js.js
+++ js/js.js
@@ -4,9 +4,18 @@ const diminuir = document.getElementById("diminuir")
 const contador  = document.getElementById("contador")
 let conta = 0
 
+const nome = prompt ("qual é o seu nome :")
+
+const idade = Number(prompt("qual a sua idade"))
+alert (`bem vindo ${nome}  sua idade é ${idade} anos` )
+
+
 crescer.onclick = function(){
     conta++
     contador.textContent = conta
+
+    verificar()
+
 }
 
 diminuir.onclick = function(){
@@ -19,6 +28,12 @@ reset.onclick = function(){
     contador.textContent = conta
 }
 
+
+function verificar (){
+   if(conta == 50){return alert("este número é o máximo")}
+
+
+}
 
 
 
